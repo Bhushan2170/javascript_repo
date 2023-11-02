804753f46e49ae1757bf2b8ed7cac09674a9ebe9
Bhushan2170 <142440520+Bhushan2170@users.noreply.github.com>
2023-11-02 08:53:43 +0000

    conversionof data types

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score="hitesh"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
